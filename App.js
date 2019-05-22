e8eecc81ac5e9c28ad53484039a544d44e6ec887
Warto Nur Prasetyo <wartonurprasetyo@student.uns.ac.id>
2019-05-22 15:44:45 +0700

    Update routing

--- App.js
+++ App.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import Calendars from './calendar/calendars.js';
+import Event from './event/event.js';
+import Events from './event/events.js';
 
-export default class App extends React.Component {
-  render() {
-    const today = new Date();
-    return (
-      <View style={styles.container}>
-        <Calendar
-          current={today}
-          minDate={today.setFullYear(today.getFullYear() - 10)}
-          maxDate={today.setFullYear(today.getFullYear() + 10)}
-        />
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+const MainNavigator = createStackNavigator({
+  Home: { screen: Calendars },
+  Event: { screen: Event },
+  Events: { screen: Events }
 });
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
